edf6d5c2ab5a233bac7344d27b01c658c04400aa
Siraj <sirajchokshi@gmail.com>
2020-09-03 13:37:12 -0500

    docs: clarify component structure

--- components/Table.js
+++ components/Table.js
@@ -9,6 +9,8 @@ const Table = () => {
 
         Make a GET request to `https://founders-takehome-api.herokuapp.com/api/fetch` (initialized as `URL`)
         Process response data into an HTML table
+
+        Feel free to delegate to as many components as you like or make Table a class-based components.
     */
 
     return (
